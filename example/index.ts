41453acd449062cf684a2a5ff40d793ce8e21d68
Dmitry Matveev <dmitrymatveevnz@gmail.com>
2022-01-06 11:20:46 +1300

    use example for dev script

---
+++ example/index.ts
@@ -0,0 +1,31 @@
+import { server } from '../src';
+
+server.start([
+  (schema) => {
+    schema.queryType({
+      fields: (t) => ({
+        hello: t.string({
+          resolve: () => 'Hello!!!!',
+        }),
+      }),
+    });
+
+    schema.subscriptionType({
+      fields: (t) => ({
+        greetings: t.string({
+          resolve(root, args, context) {
+            console.log('resolve', context)
+            return 'event ' + root;
+          },
+          async *subscribe(root, args, context) {
+            console.log('subscribe')
+            for (var event in [0, 0, 0, 0]) {
+              yield event;
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+            }
+          },
+        }),
+      }),
+    });
+  },
+]);
