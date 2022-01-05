df881e12d2de79f9bb4a55ed836b192e0c5eafaa
Dmitry Matveev <dmitrymatveev@users.noreply.github.com>
2022-01-06 08:26:38 +1300

    Exposed necessary types to distributions. (#2)
    * add type definitions to distribution
    
    * version bump

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-
-export { SchemaProvider, Container } from './modules';
-export * as server from './server'
+export { SchemaProvider } from './modules';
+export * as server from './server';
+export * as giraphql from '@giraphql/core';
+export * as brandi from 'brandi';
