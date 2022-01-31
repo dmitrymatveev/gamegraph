ce6fdf9bb73bdce26699b72f67aa55187bd83227
Dmitry Matveev <dmitrymatveevnz@gmail.com>
2022-01-31 18:26:54 +1300

    migrate giraphQl v3 - pothos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { SchemaProvider, SchemaBuilder } from './modules';
 export { Options } from './options';
 export * as server from './server';
-export * as giraphql from '@giraphql/core';
+export * as pothos from '@pothos/core';
 export * as brandi from 'brandi';
--- src/modules.ts
+++ src/modules.ts
@@ -1,12 +1,12 @@
 // import SchemaBuilder from '@giraphql/core';
 import { Container } from 'brandi';
-import GiraphQlBuilder from '@giraphql/core';
+import GraphQlBuilder from '@pothos/core';
 import { GraphQLSchema } from 'graphql';
 
 
-export type SchemaTypes = Partial<GiraphQLSchemaTypes.UserSchemaTypes & { Context: Container }>
+export type SchemaTypes = Partial<PothosSchemaTypes.UserSchemaTypes & { Context: Container }>
 
-export type SchemaBuilder = GiraphQLSchemaTypes.SchemaBuilder<GiraphQLSchemaTypes.ExtendDefaultTypes<SchemaTypes>>
+export type SchemaBuilder = PothosSchemaTypes.SchemaBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>>
 
 /**
  * GraphQL Schema factory function.
@@ -14,7 +14,7 @@ export type SchemaBuilder = GiraphQLSchemaTypes.SchemaBuilder<GiraphQLSchemaType
 export type SchemaProvider = (builder: SchemaBuilder) => void;
 
 export const buildSchemaFromProviders = (providers: SchemaProvider[]): GraphQLSchema => {
-    const builder = new GiraphQlBuilder<{ Context: Container }>({});
+    const builder = new GraphQlBuilder<{ Context: Container }>({});
     providers.forEach(addToSchema => addToSchema(builder));
     return builder.toSchema({});
 }
