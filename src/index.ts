0676306471fc8d4daca1de9df370f4b329cf003e
Dmitry Matveev <dmitrymatveevnz@gmail.com>
2022-02-05 13:01:33 +1300

    shortcut types for queries, subscriptions and mutations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-export { SchemaProvider, SchemaBuilder } from './modules';
+export {
+  ApplicationContext,
+  SchemaProvider,
+  SchemaBuilder,
+  QueryBuilder,
+  MutationBuilder,
+  SubscriptionBuilder
+} from './modules';
 export { Options } from './options';
 export * as server from './server';
 export * as pothos from '@pothos/core';
