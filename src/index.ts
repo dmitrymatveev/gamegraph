9c1f1f31478776a92486d3da1164372a7c675e04
Dmitry Matveev <dmitrymatveevnz@gmail.com>
2022-02-05 13:01:33 +1300

    shortcut types for queries, subscriptions and mutations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-export { SchemaProvider, SchemaBuilder } from './modules';
+export {
+  ApplicationContext,
+  SchemaProvider,
+  SchemaBuilder,
+  QueryBuilder,
+  MutationBuilder,
+  SubscriptionBuilder,
+  ObjectBuilder,
+} from './modules';
 export { Options } from './options';
 export * as server from './server';
 export * as pothos from '@pothos/core';
--- src/modules.ts
+++ src/modules.ts
@@ -3,9 +3,19 @@ import { Container } from 'brandi';
 import GraphQlBuilder from '@pothos/core';
 import { GraphQLSchema } from 'graphql';
 
-export type SchemaTypes = Partial<PothosSchemaTypes.UserSchemaTypes & { Context: Container }>
+export type ApplicationContext = { Context: Container }
+
+export type SchemaTypes = Partial<PothosSchemaTypes.UserSchemaTypes & ApplicationContext>
 
 export type SchemaBuilder = PothosSchemaTypes.SchemaBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>>
+
+export type QueryBuilder = PothosSchemaTypes.QueryFieldBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>, object & {}>
+
+export type MutationBuilder = PothosSchemaTypes.MutationFieldBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>, object & {}>
+
+export type SubscriptionBuilder = PothosSchemaTypes.SubscriptionFieldBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>, object & {}>
+
+export type ObjectBuilder = PothosSchemaTypes.QueryFieldBuilder<PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>, object & {}>
 
 /**
  * GraphQL Schema factory function.
@@ -13,7 +23,7 @@ export type SchemaBuilder = PothosSchemaTypes.SchemaBuilder<PothosSchemaTypes.Ex
 export type SchemaProvider = (builder: SchemaBuilder) => void;
 
 export const buildSchemaFromProviders = (providers: SchemaProvider[]): GraphQLSchema => {
-    const builder = new GraphQlBuilder<{ Context: Container }>({});
+    const builder = new GraphQlBuilder<ApplicationContext>({});
     providers.forEach(addToSchema => addToSchema(builder));
     return builder.toSchema({});
 }
