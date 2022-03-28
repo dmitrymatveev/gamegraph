6a6f6a45a86eb52306e0ea0eb9d9272c86cffde2
Dmitry Matveev <dmitrymatveevnz@gmail.com>
2022-03-28 23:45:09 +1300

    add registerDependencies optional callback

--- src/middleware/graphQlMiddleware.ts
+++ src/middleware/graphQlMiddleware.ts
@@ -18,40 +18,50 @@ export type GraphQlMiddlewareOptions<TContext> = Options<TContext> & {
 
 export const graphQlMiddleware: <TContext>(
   context: GraphQlMiddlewareOptions<TContext>
-) => RequestHandler = (context) => async (req, res) => {
-  const { serveGrip, schema, renderDocs } = context;
+) => RequestHandler = (context) => {
 
-  // Run the middleware
-  if (!(await serveGrip.run(req, res))) {
-    // If serveGrip.run() has returned false, it means the middleware has already
-    // sent and ended the response, usually due to an error.
-    return;
+  const { registerDependencies } = context;
+  const rootContainer = new Container();
+
+  if (registerDependencies) {
+    registerDependencies(rootContainer);
   }
-
-  const request = {
-    body: req.body,
-    headers: req.headers,
-    method: req.method,
-    query: req.query,
+  
+  return async (req, res) => {
+    const { serveGrip, schema, renderDocs } = context;
+  
+    // Run the middleware
+    if (!(await serveGrip.run(req, res))) {
+      // If serveGrip.run() has returned false, it means the middleware has already
+      // sent and ended the response, usually due to an error.
+      return;
+    }
+  
+    const request = {
+      body: req.body,
+      headers: req.headers,
+      method: req.method,
+      query: req.query,
+    };
+  
+    if (renderDocs && shouldRenderGraphiQL(request)) {
+      res.send(renderGraphiQL());
+      return;
+    }
+  
+    const { operationName, query, variables } = getGraphQLParameters(request);
+  
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema,
+      contextFactory: () => ({
+        container: new Container().extend(rootContainer)
+      }),
+    });
+  
+    sendResult(result, res);
   };
-
-  if (renderDocs && shouldRenderGraphiQL(request)) {
-    res.send(renderGraphiQL());
-    return;
-  }
-
-  const { operationName, query, variables } = getGraphQLParameters(request);
-
-  const result = await processRequest({
-    operationName,
-    query,
-    variables,
-    request,
-    schema,
-    contextFactory: () => ({
-      container: new Container()
-    }),
-  });
-
-  sendResult(result, res);
-};
+}
